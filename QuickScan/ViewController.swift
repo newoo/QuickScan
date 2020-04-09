d5425d35c42f2c3341146d9a6fd9319e24c3e0d5
newoo <45201399+newoo@users.noreply.github.com>
2020-04-10 00:11:29 +0900

    Initial Commit

---
+++ QuickScan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickScan
+//
+//  Created by Taeheon Woo on 2020/04/10.
+//  Copyright © 2020 newoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
